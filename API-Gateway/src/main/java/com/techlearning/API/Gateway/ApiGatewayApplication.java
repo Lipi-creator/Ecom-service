e83d9e537dd343e78d7ef1a312d88e96bb8c58bc
lipibanerjee <lbanerjee805@gmail.com>
2025-07-14 13:07:55 +0530

    Implement API Gateway and config server

--- API-Gateway/src/main/java/com/techlearning/API/Gateway/ApiGatewayApplication.java
+++ API-Gateway/src/main/java/com/techlearning/API/Gateway/ApiGatewayApplication.java
@@ -3,6 +3,10 @@ package com.techlearning.API.Gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Challenges - Communication issue : client must remember every service URL
+// 2 - Request must be done to different services
+// 3 - Security and authentication
+// 4 - To create one entry point for client
 @SpringBootApplication
 public class ApiGatewayApplication {
 
